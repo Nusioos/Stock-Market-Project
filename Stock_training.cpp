8f7a2d1ae4348fb3e4c0a3479c7c0c00115bd340
Nusioos <136240793+Nusioos@users.noreply.github.com>
2025-10-11 13:51:04 +0200

    Added tuple and Demo list of cryptocurencies
    As mentioned in title

--- Stock_training.cpp
+++ Stock_training.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <sqlite3.h>
 #include <cstdio> 
+#include <vector>
 using namespace std;
-class SQLmanager
+
+class SQLmanager 
 {
 public:
 struct stock
@@ -19,9 +21,7 @@ void Insert_to_table(stock row,sqlite3 *db,char *errMsg)
     if (sqlite3_exec(db, query.c_str(), nullptr, nullptr, &errMsg) != SQLITE_OK) {
        cout << "Blad INSERT: " << errMsg << endl;
         sqlite3_free(errMsg);
-    } else {
-        cout << "Dodano dane!" << endl;
-    }
+    } 
 }
 
 void Delete_database()
@@ -34,6 +34,24 @@ if (result == 0) {
 }
 }
 };
+ class SQL_list_of_stocks :SQLmanager
+{
+public:
+    vector<tuple<string, int>> StockList = {
+        {"Bitcoin", 30000},
+        {"Ethereum", 2000},
+        {"Cardano", 1},
+        {"Solana", 35},
+        {"Polkadot", 20},
+        {"Dogecoin", 0},
+        {"Litecoin", 150},
+        {"Avalanche", 25},
+        {"Chainlink", 15},
+        {"Shiba", 0}
+    };
+};
+
+
 int main() {
     sqlite3 *db;
     char *errMsg =nullptr;
@@ -58,19 +76,24 @@ int main() {
     }
 
     SQLmanager* manager=new SQLmanager();
+    SQL_list_of_stocks Lista;
 
-    manager->s.symbol="ETH";
-    manager->s.price=4200;
+ for(int i=0;i<Lista.StockList.size();i++)
+  {
+    manager->s.symbol = get<0>(Lista.StockList[i]); //get for the tupples 
+    manager->s.price  = get<1>(Lista.StockList[i]); 
     manager->Insert_to_table(manager->s,db,errMsg);
-   //manager->Delete_database();
+  }
 
-  const char *insertSQL = "INSERT INTO stocks (symbol, price) VALUES ('BTC', 65000.0);";
+ //  manager->Delete_database(); //DELETES WHOLE DATABASE
+
+  /*const char *insertSQL = "INSERT INTO stocks (symbol, price) VALUES ('BTC', 65000.0);";
     if (sqlite3_exec(db, insertSQL, nullptr, nullptr, &errMsg) != SQLITE_OK) {
         std::cerr << "Błąd INSERT: " << errMsg << std::endl;
         sqlite3_free(errMsg);
     } else {
         std::cout << "Dodano dane!" << std::endl;
-    }
+    }*/
        auto callback = [](void*, int argc, char **argv, char **colNames) -> int {
         std::cout << "Wynik:" << std::endl;
         for (int i = 0; i < argc; i++) {
@@ -79,6 +102,7 @@ int main() {
         std::cout << "-------------------" << std::endl;
         return 0;
     };
+
 
 
      auto find =[](void*,int argc,char **argv,char **colNames)->int
