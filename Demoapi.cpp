407840d7dfb022c445fdd8381830f36dcf0e5626
Nusio_coding <136240793+Nusioos@users.noreply.github.com>
2025-10-18 00:23:36 +0200

    further api testing

--- Demoapi.cpp
+++ Demoapi.cpp
@@ -1,37 +1,56 @@
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
-#include "nlohmann/json.hpp" // <- Zwróć uwagę na <>, nie ""
-
+#include "nlohmann/json.hpp" 
+#include <chrono>
+#include <thread>
+#include<mutex>
+using namespace std;
 using json = nlohmann::json;
-
+std::mutex mtx;  // mutex do synchronizacji
+double btc_price = 0.0;
 // Funkcja callback do zapisywania danych pobranych przez CURL
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
-
-int main() {
+void Get_values()
+{
     CURL* curl;
     CURLcode res;
-    std::string readBuffer;
-
     curl = curl_easy_init();
-    if(curl) {
+    if(!curl) return;
+
+    while(true) {
+        std::string readBuffer;
         curl_easy_setopt(curl, CURLOPT_URL, "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    //    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
 
         res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-
         if(res == CURLE_OK) {
             json j = json::parse(readBuffer);
-            double btc_price = j["bitcoin"]["usd"];
-            std::cout << "Cena BTC: " << btc_price << " USD" << std::endl;
-        } else {
-            std::cerr << "Błąd pobierania danych: " << curl_easy_strerror(res) << std::endl;
+            std::lock_guard<std::mutex> lock(mtx);
+            btc_price = j["bitcoin"]["usd"];
         }
+
+        curl_easy_cleanup(curl);
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // pobieranie co 1 sekundę
     }
-    return 0;
 }
+
+int main() {
+    std::thread background(Get_values); 
+    background.detach();
+
+    while(true) {
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            std::cout << "Aktualna cena BTC: " << btc_price << " USD" << std::endl;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "sperma" << std::endl;
+    }
+}
+
