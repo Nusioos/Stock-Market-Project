f1e82368f9aac4683dfd6e8b99f6d8bd2c32959c
Nusioos <136240793+Nusioos@users.noreply.github.com>
2025-10-20 01:23:19 +0200

    Bit coin graph

--- Demoapi.cpp
+++ Demoapi.cpp
@@ -5,52 +5,112 @@
 #include <chrono>
 #include <thread>
 #include<mutex>
+#include <deque>
 using namespace std;
 using json = nlohmann::json;
-std::mutex mtx;  // mutex do synchronizacji
+mutex mtx;  
 double btc_price = 0.0;
-// Funkcja callback do zapisywania danych pobranych przez CURL
+deque<size_t> btc_price_dequeue;
+void Generate_graph(size_t Bitcoin_prize,deque<size_t> Stocks,size_t maksprice);
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 void Get_values()
 {
-    CURL* curl;
-    CURLcode res;
-    curl = curl_easy_init();
-    if(!curl) return;
+ 
+    while (true) {
+           
+        CURL* curl = curl_easy_init();
+        if (curl) {
+            std::string readBuffer;
+            curl_easy_setopt(curl, CURLOPT_URL, "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 
-    while(true) {
-        std::string readBuffer;
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-    //    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
+            CURLcode res = curl_easy_perform(curl);
+            if (res == CURLE_OK) {
+                int latest=0;
+          try{
+                    json j = json::parse(readBuffer);
+                     lock_guard<mutex> lock(mtx);
+                    btc_price = j["bitcoin"]["usd"];
+                    if(btc_price_dequeue.size()>60)
+                    {
+                         btc_price_dequeue.pop_back();
+                    }
+                    btc_price_dequeue.push_front((size_t)btc_price);
+                   
+          }
+          catch(exception& e)
+          {
+          cout << "blad" << endl;
+          }
 
-        res = curl_easy_perform(curl);
-        if(res == CURLE_OK) {
-            json j = json::parse(readBuffer);
-            std::lock_guard<std::mutex> lock(mtx);
-            btc_price = j["bitcoin"]["usd"];
+     
+            } else {
+                cerr << "Błąd CURL: " << curl_easy_strerror(res) << endl;
+            }
+
+            curl_easy_cleanup(curl);
         }
 
-        curl_easy_cleanup(curl);
-        std::this_thread::sleep_for(std::chrono::seconds(1)); // pobieranie co 1 sekundę
+        std::this_thread::sleep_for(std::chrono::seconds(15)); 
     }
 }
 
-int main() {
-    std::thread background(Get_values); 
-    background.detach();
 
+
+int main() {
+    double curr=0;
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    thread background(Get_values); 
+    background.detach();
+ std::this_thread::sleep_for(std::chrono::seconds(10)); 
     while(true) {
         {
-            std::lock_guard<std::mutex> lock(mtx);
+            lock_guard<mutex> lock(mtx);
             std::cout << "Aktualna cena BTC: " << btc_price << " USD" << std::endl;
+            size_t maks_price=max(curr,btc_price);
+           Generate_graph(btc_price,btc_price_dequeue,maks_price);
+           
+
         }
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::seconds(20));
         std::cout << "sperma" << std::endl;
     }
 }
+void Generate_graph(size_t Bitcoin_prize, deque<size_t> Stocks, size_t maksprice)
+{
+    size_t minPrice = Stocks[0];
+    size_t maxPrice = Stocks[0];
+    const int HEIGHT=20;
+    for (auto p : Stocks) {
+        if (p < minPrice) minPrice = p;
+        if (p > maxPrice) maxPrice = p;
+    }
 
+
+    for (int y = HEIGHT; y >= 0; y--) {
+        cout << "|";
+        for (size_t x = 0; x < Stocks.size(); x++) {
+            size_t sum;
+            if (maxPrice != minPrice)
+                sum = (Stocks[x] - minPrice) * HEIGHT / (maxPrice - minPrice);
+            else
+                sum = HEIGHT;
+
+            if (sum == y)
+                cout << "x";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    // Oś X
+    cout << "+";
+    for (size_t i = 0; i < Stocks.size(); i++)
+        cout << "-";
+    cout << endl;
+}
